9a5957c40179d4a9e396da9aede345af52b0ae0d
ToTiPC <muaxheritoti@gmail.com>
2023-08-01 19:00:04 +0200

    video72

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +15,22 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args){
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 
         return runner -> {
-            System.out.println("Hello World");
+           createStudent(studentDAO);
         };
+    }2
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating new student object...");
+        Student tempStudent = new Student("Toti", "Muhaxheri", "toti@gmail.com");
+
+
+        System.out.println("Saving the student...");
+        studentDAO.save(tempStudent);
+
+        System.out.println("Saved student. generated id: "+ tempStudent.getId());
     }
 
 }
